93a8424bed10859ae594b0882af6f84de0b5949c
PhanThiTuongVy0211 <tuongvyphan0211travinh@211travinhgmail.com>
2025-11-19 18:49:22 +0700

    [HTML_client]header &footer

--- SrcCatGiaoDien/components/single-product/info.php
+++ SrcCatGiaoDien/components/single-product/info.php
@@ -1,1 +1,62 @@
 <h1>Chi tiết sản phẩm</h1>
+
+ <div class="d-flex align-items-center justify-content-between mt-4 mx-2">
+    <h4>Nổi bật hôm nay</h4>
+    <button class="btn btn-outline-success">View</button>
+  </div>
+  <div class="row mx-2">
+    <div class="col-3">
+      <ul class="list-group mt-3">
+        <li class="list-group-item btn-brown" aria-current="true">
+          Tìm kiếm sản phẩm
+        </li>
+        <li class="list-group-item">
+          <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </li>
+      </ul>
+      <ul class="list-group mt-3">
+        <li class="list-group-item btn-brown " aria-current="true">
+          Danh mục sản phẩm
+        </li>
+        <li class="list-group-item"></li>Cải đà lạt
+        <li class="list-group-item">Dâu tây</li>
+        <li class="list-group-item">Rau muống</li>
+        <li class="list-group-item">Rau hữu cơ</li>
+      </ul>
+    </div>
+    <div class="col-9">
+      <div class="row">
+        <div
+          v-for="item in products"
+          :key="item.id"
+          class="col-lg-4 col-md-6 mt-3"
+        >
+          <div class="card">
+            <img
+              :src="item.image"
+              class="card-img-top"
+              alt="..."
+              style="height: 250px"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Cà chua</h5>
+              <p class="card-price">
+                80.000vnd
+              </p>
+              <a href="#" class="btn btn-brown">Mua ngay</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
