87ee6237881dc58efc872cbd32a19efb5aa4f7fa
Hieuntpc08839 <hieuntpc08839@gmail.com>
2025-11-24 13:23:32 +0700

    [HTML] [Client] -Trang About Us và trang blog

---
+++ SrcCatGiaoDien/about.php
@@ -0,0 +1,171 @@
+
+    <div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-fullscreen">
+            <div class="modal-content rounded-0">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Search by keyword</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body d-flex align-items-center">
+                    <div class="input-group w-75 mx-auto d-flex">
+                        <input type="search" class="form-control p-3" placeholder="keywords" aria-describedby="search-icon-1">
+                        <span id="search-icon-1" class="input-group-text p-3"><i class="fa fa-search"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid page-header py-5">
+        <h1 class="text-center text-white display-6">About Us</h1>
+        <ol class="breadcrumb justify-content-center mb-0">
+            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+            <li class="breadcrumb-item"><a href="#">Pages</a></li>
+            <li class="breadcrumb-item active text-white">About</li>
+        </ol>
+    </div>
+    <div class="container-fluid py-5">
+        <div class="container py-5">
+            <div class="row g-5 align-items-center">
+                <div class="col-lg-6">
+                    <div class="row g-4">
+                        <div class="col-6">
+                            <img src="img/best-product-1.jpg" class="img-fluid rounded w-100" alt="">
+                        </div>
+                        <div class="col-6">
+                            <img src="img/best-product-2.jpg" class="img-fluid rounded w-100 mb-4" alt="">
+                            <img src="img/best-product-3.jpg" class="img-fluid rounded w-100" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <h1 class="mb-4">Welcome to <span class="text-primary">Fruitables</span></h1>
+                    <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum officia hic culpa sunt aut similique accusantium id, labore, ullam incidunt commodi laborum distinctio, temporibus voluptatum officiis. Doloremque, officiis.</p>
+                    <p class="mb-4">Temporibus voluptatum officiis. Doloremque, officiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, labore! Eum officia hic culpa sunt aut similique accusantium id.</p>
+                    
+                    <div class="row g-4 mb-4">
+                        <div class="col-md-6">
+                            <div class="d-flex align-items-center">
+                                <i class="fas fa-seedling fa-3x text-primary me-3"></i>
+                                <div>
+                                    <h5 class="mb-0">100% Organic</h5>
+                                    <p class="mb-0 text-secondary">Fresh from farm</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="d-flex align-items-center">
+                                <i class="fas fa-truck fa-3x text-primary me-3"></i>
+                                <div>
+                                    <h5 class="mb-0">Free Delivery</h5>
+                                    <p class="mb-0 text-secondary">On order over $50</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <a href="shop.html" class="btn btn-primary rounded-pill py-3 px-5">Shop Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid bg-secondary my-5 py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-lg-3 text-center">
+                    <i class="fa fa-users fa-3x text-primary mb-3"></i>
+                    <h2 class="text-white mb-2" data-toggle="counter-up">120+</h2>
+                    <p class="text-white mb-0">Satisfied Customers</p>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center">
+                    <i class="fa fa-users fa-3x text-primary mb-3"></i>
+                    <h2 class="text-white mb-2" data-toggle="counter-up">15+</h2>
+                    <p class="text-white mb-0">Years of Experience</p>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center">
+                    <i class="fa fa-certificate fa-3x text-primary mb-3"></i>
+                    <h2 class="text-white mb-2" data-toggle="counter-up">50+</h2>
+                    <p class="text-white mb-0">Awards Won</p>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center">
+                    <i class="fa fa-shield-alt fa-3x text-primary mb-3"></i>
+                    <h2 class="text-white mb-2" data-toggle="counter-up">50+</h2>
+                    <p class="text-white mb-0">Team Members</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid py-5">
+        <div class="container py-5">
+            <div class="text-center mx-auto mb-5" style="max-width: 700px;">
+                <h1 class="display-4">Our Professional Team</h1>
+                <p>Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.</p>
+            </div>
+            <div class="row g-4">
+                <div class="col-lg-3 col-md-6">
+                    <div class="team-item position-relative rounded">
+                        <div class="rounded-top overflow-hidden">
+                            <img src="img/testimonial-1.jpg" class="img-fluid w-100 rounded-top bg-light" alt="">
+                        </div>
+                        <div class="team-text bg-light text-center p-4">
+                            <h5>Jason Smith</h5>
+                            <p class="mb-0">Farmer</p>
+                            <div class="team-social d-flex justify-content-center pt-3">
+                                <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-twitter"></i></a>
+                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="team-item position-relative rounded">
+                        <div class="rounded-top overflow-hidden">
+                            <img src="img/testimonial-2.jpg" class="img-fluid w-100 rounded-top bg-light" alt="">
+                        </div>
+                        <div class="team-text bg-light text-center p-4">
+                            <h5>Linda Green</h5>
+                            <p class="mb-0">Manager</p>
+                            <div class="team-social d-flex justify-content-center pt-3">
+                                <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-twitter"></i></a>
+                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="team-item position-relative rounded">
+                        <div class="rounded-top overflow-hidden">
+                            <img src="img/testimonial-1.jpg" class="img-fluid w-100 rounded-top bg-light" alt="">
+                        </div>
+                        <div class="team-text bg-light text-center p-4">
+                            <h5>Tom Brown</h5>
+                            <p class="mb-0">Sales</p>
+                            <div class="team-social d-flex justify-content-center pt-3">
+                                <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-twitter"></i></a>
+                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="team-item position-relative rounded">
+                        <div class="rounded-top overflow-hidden">
+                            <img src="img/testimonial-2.jpg" class="img-fluid w-100 rounded-top bg-light" alt="">
+                        </div>
+                        <div class="team-text bg-light text-center p-4">
+                            <h5>Emily White</h5>
+                            <p class="mb-0">Support</p>
+                            <div class="team-social d-flex justify-content-center pt-3">
+                                <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-twitter"></i></a>
+                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
