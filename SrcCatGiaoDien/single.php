1f2dc7e1f41cf2262f32f8b57b1dcf5667867668
Kimanh278 <kimanhdang278@gmail.com>
2025-11-30 21:14:57 +0700

    [Html_Client]Cart giỏ hàng

---
+++ SrcCatGiaoDien/single.php
@@ -0,0 +1,15 @@
+<?php
+require_once "header.php";
+$array = [
+    "components/layouts/header.php",
+    // "components/shop/navbar.php",
+    "components/single-product/cart.php",
+
+    "components/layouts/footer.php",
+];
+
+foreach ($array as $component) {
+    require_once($component);
+}
+
+require_once "footer.php";
