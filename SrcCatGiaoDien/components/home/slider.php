b9a365ba796eda4518a3794c24e10e343cdf1e83
Kimanh278 <kimanhdang278@gmail.com>
2025-11-30 20:51:28 +0700

    [Client_Html] Trang chủ home

--- SrcCatGiaoDien/components/home/slider.php
+++ SrcCatGiaoDien/components/home/slider.php
@@ -1,1 +1,37 @@
-<h1>Slider</h1>
+
+  <div
+    id="carouselExampleControls"
+    class="carousel slide"
+    data-bs-ride="carousel"
+  >
+    <div class="carousel-inner" >
+      <div class="carousel-item active" style="height: 450px;">
+        <img src="https://i.pinimg.com/1200x/2b/c7/78/2bc77846a367f14cbe3483a0353f7ced.jpg" class="d-block w-100" alt="..." />
+      </div>
+      <div class="carousel-item" style="height: 450px;">
+        <img src="https://i.pinimg.com/736x/a3/6c/84/a36c842042e3828d6f1ba598363ac88e.jpg" class="d-block w-100" alt="..." />
+      </div>
+      <div class="carousel-item" style="height: 450px;">
+        <img src="https://i.pinimg.com/1200x/7f/ac/9a/7fac9a7ac30962d1f333918d0b734a3c.jpg" class="d-block w-100" alt="..." />
+      </div>
+    </div>
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#carouselExampleControls"
+      data-bs-slide="prev"
+    >
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselExampleControls"
+      data-bs-slide="next"
+    >
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
---
+++ SrcCatGiaoDien/components/home/tastimonial.php
@@ -0,0 +1,87 @@
+ <!-- Tastimonial Start -->
+        <div class="container-fluid testimonial py-5">
+            <div class="container py-5">
+                <div class="testimonial-header text-center">
+                    <h4 class="text-primary">Lời chứng thực của chúng tôi</h4>
+                    <h1 class="display-5 mb-5 text-dark">Khách hàng của chúng tôi nói!</h1>
+                </div>
+                <div class="owl-carousel testimonial-carousel">
+                    <div class="testimonial-item img-border-radius bg-light rounded p-4">
+                        <div class="position-relative">
+                            <i class="fa fa-quote-right fa-2x text-secondary position-absolute" style="bottom: 30px; right: 0;"></i>
+                            <div class="mb-4 pb-4 border-bottom border-secondary">
+                                <p class="mb-0">Sản phẩm rất ngon và ok, chất lượng đảm bảo an toàn co người tiêu dùng, sản phẩm tươi ngon chất lượng được nhập và kiểm định.
+                                </p>
+                            </div>
+                            <div class="d-flex align-items-center flex-nowrap">
+                                <div class="bg-secondary rounded">
+                                    <img src="assets/img/testimonial-1.jpg" class="img-fluid rounded" style="width: 100px; height: 100px;" alt="">
+                                </div>
+                                <div class="ms-4 d-block">
+                                    <h4 class="text-dark">Tường Vy</h4>
+                                    <p class="m-0 pb-3">Đánh giá</p>
+                                    <div class="d-flex pe-5">
+                                        <i class="fas fa-star text-primary"></i>
+                                        <i class="fas fa-star text-primary"></i>
+                                        <i class="fas fa-star text-primary"></i>
+                                        <i class="fas fa-star text-primary"></i>
+                                        <i class="fas fa-star"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="testimonial-item img-border-radius bg-light rounded p-4">
+                        <div class="position-relative">
+                            <i class="fa fa-quote-right fa-2x text-secondary position-absolute" style="bottom: 30px; right: 0;"></i>
+                            <div class="mb-4 pb-4 border-bottom border-secondary">
+                                <p class="mb-0">Sản phẩm rất ngon và ok, chất lượng đảm bảo an toàn co người tiêu dùng, sản phẩm tươi ngon chất lượng được nhập và kiểm định.
+                                </p>
+                            </div>
+                            <div class="d-flex align-items-center flex-nowrap">
+                                <div class="bg-secondary rounded">
+                                    <img src="assets/img/testimonial-1.jpg" class="img-fluid rounded" style="width: 100px; height: 100px;" alt="">
+                                </div>
+                                <div class="ms-4 d-block">
+                                    <h4 class="text-dark">Kim Anh</h4>
+                                    <p class="m-0 pb-3">Đánh giá</p>
+                                    <div class="d-flex pe-5">
+                                        <i class="fas fa-star text-primary"></i>
+                                        <i class="fas fa-star text-primary"></i>
+                                        <i class="fas fa-star text-primary"></i>
+                                        <i class="fas fa-star text-primary"></i>
+                                        <i class="fas fa-star text-primary"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="testimonial-item img-border-radius bg-light rounded p-4">
+                        <div class="position-relative">
+                            <i class="fa fa-quote-right fa-2x text-secondary position-absolute" style="bottom: 30px; right: 0;"></i>
+                            <div class="mb-4 pb-4 border-bottom border-secondary">
+                                <p class="mb-0">Sản phẩm rất ngon và ok, chất lượng đảm bảo an toàn co người tiêu dùng, sản phẩm tươi ngon chất lượng được nhập và kiểm định.
+                                </p>
+                            </div>
+                            <div class="d-flex align-items-center flex-nowrap">
+                                <div class="bg-secondary rounded">
+                                    <img src="assets/img/testimonial-1.jpg" class="img-fluid rounded" style="width: 100px; height: 100px;" alt="">
+                                </div>
+                                <div class="ms-4 d-block">
+                                    <h4 class="text-dark">Hiếu</h4>
+                                    <p class="m-0 pb-3">Đánh giá</p>
+                                    <div class="d-flex pe-5">
+                                        <i class="fas fa-star text-primary"></i>
+                                        <i class="fas fa-star text-primary"></i>
+                                        <i class="fas fa-star text-primary"></i>
+                                        <i class="fas fa-star text-primary"></i>
+                                        <i class="fas fa-star text-primary"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Tastimonial End -->
