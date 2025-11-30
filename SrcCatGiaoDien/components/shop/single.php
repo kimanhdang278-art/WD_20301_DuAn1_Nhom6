f823d194a6899f73eb73eef9d958b6e330d981cf
Kimanh278 <kimanhdang278@gmail.com>
2025-11-30 21:01:31 +0700

    [Html_Client] Trang Shop, sản phẩm

---
+++ SrcCatGiaoDien/components/shop/single.php
@@ -0,0 +1,10 @@
+ <!-- Single Page Header start -->
+        <div class="container-fluid page-header py-5">
+            <h1 class="text-center text-white display-6">Sản phẩm</h1>
+            <ol class="breadcrumb justify-content-center mb-0">
+                <li class="breadcrumb-item"><a href="#">Trang chủ</a></li>
+                <li class="breadcrumb-item"><a href="#">Trang</a></li>
+                <li class="breadcrumb-item active text-white">Sản phẩm</li>
+            </ol>
+        </div>
+        <!-- Single Page Header End -->
