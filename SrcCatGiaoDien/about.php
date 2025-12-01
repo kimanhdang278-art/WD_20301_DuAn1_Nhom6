8c262af50605aa3152fa972fb358f16df915cd07
Hieuntpc08839 <hieuntpc08839@gmail.com>
2025-12-01 13:38:04 +0700

    [Client][HTML] - giao diện trang about

---
+++ SrcCatGiaoDien/about.php
@@ -0,0 +1,178 @@
+<div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-fullscreen">
+        <div class="modal-content rounded-0">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tìm kiếm bằng từ khóa</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Đóng"></button>
+            </div>
+            <div class="modal-body d-flex align-items-center">
+                <div class="input-group w-75 mx-auto d-flex">
+                    <input type="search" class="form-control p-3" placeholder="Nhập từ khóa..." aria-describedby="search-icon-1">
+                    <span id="search-icon-1" class="input-group-text p-3"><i class="fa fa-search"></i></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid page-header py-5">
+    <h1 class="text-center text-white display-6">Giới Thiệu</h1>
+    <ol class="breadcrumb justify-content-center mb-0">
+        <li class="breadcrumb-item"><a href="index.html">Trang Chủ</a></li>
+        <li class="breadcrumb-item"><a href="#">Trang</a></li>
+        <li class="breadcrumb-item active text-white">Giới Thiệu</li>
+    </ol>
+</div>
+
+<div class="container-fluid py-5">
+    <div class="container py-5">
+        <div class="row g-5 align-items-center">
+            <div class="col-lg-6">
+                <div class="row g-4">
+                    <div class="col-6">
+                        <img src="img/best-product-1.jpg" class="img-fluid rounded w-100" alt="">
+                    </div>
+                    <div class="col-6">
+                        <img src="img/best-product-2.jpg" class="img-fluid rounded w-100 mb-4" alt="">
+                        <img src="img/best-product-3.jpg" class="img-fluid rounded w-100" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+                <h1 class="mb-4">Chào mừng đến với <span class="text-primary">Fruitables</span></h1>
+                <p class="mb-4">
+                    Chúng tôi tự hào là cửa hàng nông sản sạch và trái cây hữu cơ uy tín, cung cấp sản phẩm chất lượng cao cho mọi gia đình Việt Nam.
+                </p>
+                <p class="mb-4">
+                    Với nhiều năm kinh nghiệm trong ngành, chúng tôi cam kết mang đến thực phẩm tươi ngon, an toàn và dịch vụ tận tâm.
+                </p>
+
+                <div class="row g-4 mb-4">
+                    <div class="col-md-6">
+                        <div class="d-flex align-items-center">
+                            <i class="fas fa-seedling fa-3x text-primary me-3"></i>
+                            <div>
+                                <h5 class="mb-0">100% Hữu Cơ</h5>
+                                <p class="mb-0 text-secondary">Tươi sạch từ nông trại</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="d-flex align-items-center">
+                            <i class="fas fa-truck fa-3x text-primary me-3"></i>
+                            <div>
+                                <h5 class="mb-0">Giao Hàng Miễn Phí</h5>
+                                <p class="mb-0 text-secondary">Đơn hàng trên 1.200.000₫</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <a href="shop.html" class="btn btn-primary rounded-pill py-3 px-5">Mua Ngay</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid bg-secondary my-5 py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-lg-3 text-center">
+                <i class="fa fa-users fa-3x text-primary mb-3"></i>
+                <h2 class="text-white mb-2" data-toggle="counter-up">120+</h2>
+                <p class="text-white mb-0">Khách Hàng Hài Lòng</p>
+            </div>
+
+            <div class="col-md-6 col-lg-3 text-center">
+                <i class="fa fa-users fa-3x text-primary mb-3"></i>
+                <h2 class="text-white mb-2" data-toggle="counter-up">15+</h2>
+                <p class="text-white mb-0">Năm Kinh Nghiệm</p>
+            </div>
+
+            <div class="col-md-6 col-lg-3 text-center">
+                <i class="fa fa-certificate fa-3x text-primary mb-3"></i>
+                <h2 class="text-white mb-2" data-toggle="counter-up">50+</h2>
+                <p class="text-white mb-0">Giải Thưởng Đạt Được</p>
+            </div>
+
+            <div class="col-md-6 col-lg-3 text-center">
+                <i class="fa fa-shield-alt fa-3x text-primary mb-3"></i>
+                <h2 class="text-white mb-2" data-toggle="counter-up">50+</h2>
+                <p class="text-white mb-0">Thành Viên Đội Ngũ</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid py-5">
+    <div class="container py-5">
+        <div class="text-center mx-auto mb-5" style="max-width: 700px;">
+            <h1 class="display-4">Đội Ngũ Chuyên Nghiệp Của Chúng Tôi</h1>
+            <p>
+                Những con người đầy nhiệt huyết, giàu chuyên môn và luôn sẵn sàng hỗ trợ bạn trong mọi hành trình mua sắm.
+            </p>
+        </div>
+
+        <div class="row g-4">
+
+            <div class="col-lg-3 col-md-6">
+                <div class="team-item position-relative rounded">
+                    <div class="team-text bg-light text-center p-4">
+                        <h5>Jason Smith</h5>
+                        <p class="mb-0">Nông Dân</p>
+                        <div class="team-social d-flex justify-content-center pt-3">
+                            <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="team-item position-relative rounded">
+                    <div class="team-text bg-light text-center p-4">
+                        <h5>Linda Green</h5>
+                        <p class="mb-0">Quản Lý</p>
+                        <div class="team-social d-flex justify-content-center pt-3">
+                            <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="team-item position-relative rounded">
+                    <div class="team-text bg-light text-center p-4">
+                        <h5>Tom Brown</h5>
+                        <p class="mb-0">Bán Hàng</p>
+                        <div class="team-social d-flex justify-content-center pt-3">
+                            <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="team-item position-relative rounded">
+                    <div class="team-text bg-light text-center p-4">
+                        <h5>Emily White</h5>
+                        <p class="mb-0">Hỗ Trợ Khách Hàng</p>
+                        <div class="team-social d-flex justify-content-center pt-3">
+                            <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
