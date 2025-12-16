ab0b02a024168475202a2fd6e250fcd5fcff87c9
TUONGVY <tuongvyphan0211travinh@gmail.com>
2025-12-16 16:53:38 +0700

    [CRUD] [Client] Chỉnh sửa Admin

--- Views/components/home/fruits.php
+++ Views/components/home/fruits.php
@@ -49,7 +49,7 @@
                                         <div class="col-md-6 col-lg-4 col-xl-3">
                                             <div class="rounded position-relative fruite-item">
                                                 <div class="fruite-img">
-                                                    <img src="assets/img/fruite-item-5.jpg" class="img-fluid w-100 rounded-top" alt="">
+                                                    <img src="uploads/<?= htmlspecialchars($product ['image']) ?>" class="img-fluid w-100 rounded-top" alt="">
                                                 </div>
                                                 <div class="text-white bg-secondary px-3 py-1 rounded position-absolute" style="top: 10px; left: 10px;">Trái cây</div>
                                                 <div class="p-4 border border-secondary border-top-0 rounded-bottom">
@@ -78,15 +78,15 @@
                                         <div class="col-md-6 col-lg-4 col-xl-3">
                                             <div class="rounded position-relative fruite-item">
                                                 <div class="fruite-img">
-                                                    <img src="assets/img/fruite-item-5.jpg" class="img-fluid w-100 rounded-top" alt="">
+                                                    <img src="uploads/<?= htmlspecialchars($product['image']) ?>" class="img-fluid w-100 rounded-top" alt="">
                                                 </div>
-                                                <div class="text-white bg-secondary px-3 py-1 rounded position-absolute" style="top: 10px; left: 10px;">Fruits</div>
+                                                <div class="text-white bg-secondary px-3 py-1 rounded position-absolute" style="top: 10px; left: 10px;">Rau sạch</div>
                                                 <div class="p-4 border border-secondary border-top-0 rounded-bottom">
-                                                    <h4>Grapes</h4>
-                                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
+                                                    <h4><?= htmlspecialchars($product['name']) ?></h4>
+                                                    <p><?= htmlspecialchars($product['description']) ?></p>
                                                     <div class="d-flex justify-content-between flex-lg-wrap">
-                                                        <p class="text-dark fs-5 fw-bold mb-0">$4.99 / kg</p>
-                                                        <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
+                                                        <p class="text-dark fs-5 fw-bold mb-0"><?= htmlspecialchars($product['price']) ?></p>
+                                                        <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Mua ngay</a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -248,6 +248,7 @@
                                 </div>
                             </div>
                         </div>
+                   
                     </div>
                 </div>      
             </div>
